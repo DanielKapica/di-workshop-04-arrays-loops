fd952ddd9d2fdf23edf5204ee1698a3e0ebae1d5
Daniel Kapica <kapica6105@gmail.com>
2018-10-18 12:28:56 +0100

    Created base files

---
+++ sketch/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400)
+  background(200)
+}
+
+function draw() {
+  var x = 10
+
+  rect(x, 10, 10, 10)
+  x = x + 20
+
+  rect(x, 10, 10, 10)
+  x = x + 20
+
+  rect(x, 10, 10, 10)
+  x = x + 20
+
+  rect(x, 10, 10, 10)
+  x = x + 20
+
+  rect(x, 10, 10, 10)
+  x = x + 20
+}
